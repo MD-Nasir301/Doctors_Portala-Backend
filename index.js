7808fc8437fadd14931851bbdc423d49792d6ef9
MD-Nasir301 <nasir21b@gmail.com>
2020-04-10 11:51:18 +0600

    get add

--- index.js
+++ index.js
@@ -31,6 +31,24 @@ app.get('/appointments', (req, res) => {
 })
 //
 
+// get Prescriptions
+app.get('/prescriptions', (req, res) => {
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            const collection = client.db("doctorPortal").collection("prescription");
+            collection.find().toArray((err,document) =>{ 
+                if(err){
+                    console.log(err);
+                    res.sent(err.message)
+                }else{
+                    res.send(document)
+                }
+              })
+          client.close();
+        });
+})
+//
+
 // get by date
 app.get('/appointments/:date',(req,res)=>{
     const date = req.params.date
@@ -70,6 +88,26 @@ app.get('/appointments/:date',(req,res)=>{
           client.close();
         });
     })
+
+// Add Prescription 
+    app.post('/addPrescription',(req,res)=>{
+        const client = new MongoClient(uri, { useNewUrlParser: true });
+        const prescription = req.body
+        client.connect(err => {
+            const collection = client.db("doctorPortal").collection("prescription");
+            collection.insert(prescription,(err,result) =>{ 
+                if(err){
+                    console.log(err);
+                    res.sent(err.message)
+                }else{
+                    console.log("after save data ", result.ops[0]);
+                    res.send(result.ops[0])
+                }
+              })
+          client.close();
+        });
+    })
+
 
 const port = process.env.PORT || 3200;
 app.listen(port, (err) =>{
